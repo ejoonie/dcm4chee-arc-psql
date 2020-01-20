ab1fbf94cc307b2daca4e91c0f8a5312390d959e
Youngjoon Choi <ejoonie@gmail.com>
2020-01-20 19:03:39 +0900

    WADO-RS 조회 (O) STOW-RS 안됨 (X)

--- ruby_client/dclient.rb
+++ ruby_client/dclient.rb
@@ -1,6 +1,32 @@
 require 'dicom'
+require 'rest-client'
 include DICOM
 
-node  = DClient.new('localhost', 11112, {host_ae: 'DCM4CHEE'})
-node.send('./fixture/00000002.dcm')
+# 저장
+# node  = DClient.new('localhost', 11112, {host_ae: 'DCM4CHEE'})
+# node.send('./fixture/00000002.dcm')
 
+# 저장 STOW-RS
+# https://github.com/dcm4che/dcm4chee-arc-light/wiki/Store-objects-by-STOW-RS
+# files = [
+#     File.open('./fixture/00000001.dcm', 'rb'),
+#     File.open('./fixture/00000002.dcm', 'rb'),
+#     File.open('./fixture/00000003.dcm', 'rb'),
+#     File.open('./fixture/00000004.dcm', 'rb'),
+# ]
+# begin
+# RestClient.post(
+#     'http://0.0.0.0:8080/dcm4chee-arc/aets/DCM4CHEE/rs/studies',
+#     files: files,
+#     content_type: 'application/dicom+json')
+# rescue Exception => e
+#  p(e)
+# end
+
+
+# 조회
+# https://github.com/dcm4che/dcm4chee-arc-light/wiki/RESTful-Services#wado_rs
+res = RestClient.get('http://0.0.0.0:8080/dcm4chee-arc/aets/DCM4CHEE/rs/studies')
+res = RestClient.get('http://0.0.0.0:8080/dcm4chee-arc/aets/DCM4CHEE/rs/studies/count')
+res = RestClient.get('http://0.0.0.0:8080/dcm4chee-arc/aets/DCM4CHEE/rs/instances/count')
+p res.body
