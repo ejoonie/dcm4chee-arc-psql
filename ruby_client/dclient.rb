2e5e223b6ea55445299f20faaf0779368ef2d8e9
Youngjoon Choi <ejoonie@gmail.com>
2020-03-12 19:05:23 +0900

    뷰어 환경 설정

--- ruby_client/dclient.rb
+++ ruby_client/dclient.rb
@@ -2,44 +2,45 @@ require 'dicom'
 require 'rest-client'
 include DICOM
 
-files = [
-    File.new('./fixture/00000001.dcm', 'rb'),
-    File.new('./fixture/00000002.dcm', 'rb'),
-    File.new('./fixture/00000003.dcm', 'rb'),
-    File.new('./fixture/00000004.dcm', 'rb'),
-]
+HOST = '192.168.0.32'
+# HOST = '1.237.206.121'
+# HOST = '192.168.0.6'
+# HOST = '15.165.171.194'
+PORT = '8080'
+# PORT = '8081'
+
 file_names = [
-    './fixture/00000001.dcm',
-    './fixture/00000002.dcm',
-    './fixture/00000003.dcm',
-    './fixture/00000004.dcm',
-    './fixture/00000005.dcm',
-    './fixture/00000006.dcm',
-    './fixture/00000007.dcm',
-    './fixture/00000008.dcm',
-    './fixture/00000009.dcm',
-    './fixture/00000010.dcm',
-    './fixture/00000011.dcm',
-    './fixture/00000012.dcm',
-    './fixture/00000013.dcm',
-    './fixture/00000014.dcm',
-    './fixture/00000015.dcm',
-    './fixture/00000016.dcm',
-    './fixture/00000017.dcm',
-    './fixture/00000018.dcm',
-    './fixture/00000019.dcm',
-    './fixture/00000020.dcm',
-    './fixture/00000021.dcm',
-    './fixture/00000022.dcm',
-    './fixture/00000023.dcm',
-    './fixture/00000024.dcm',
-    './fixture/00000025.dcm',
-    './fixture/00000026.dcm',
+    './fixture/ejoonie-dicom/00000001.dcm',
+    './fixture/ejoonie-dicom/00000002.dcm',
+    './fixture/ejoonie-dicom/00000003.dcm',
+    './fixture/ejoonie-dicom/00000004.dcm',
+    './fixture/ejoonie-dicom/00000005.dcm',
+    './fixture/ejoonie-dicom/00000006.dcm',
+    './fixture/ejoonie-dicom/00000007.dcm',
+    './fixture/ejoonie-dicom/00000008.dcm',
+    './fixture/ejoonie-dicom/00000009.dcm',
+    './fixture/ejoonie-dicom/00000010.dcm',
+    './fixture/ejoonie-dicom/00000011.dcm',
+    './fixture/ejoonie-dicom/00000012.dcm',
+    './fixture/ejoonie-dicom/00000013.dcm',
+    './fixture/ejoonie-dicom/00000014.dcm',
+    './fixture/ejoonie-dicom/00000015.dcm',
+    './fixture/ejoonie-dicom/00000016.dcm',
+    './fixture/ejoonie-dicom/00000017.dcm',
+    './fixture/ejoonie-dicom/00000018.dcm',
+    './fixture/ejoonie-dicom/00000019.dcm',
+    './fixture/ejoonie-dicom/00000020.dcm',
+    './fixture/ejoonie-dicom/00000021.dcm',
+    './fixture/ejoonie-dicom/00000022.dcm',
+    './fixture/ejoonie-dicom/00000023.dcm',
+    './fixture/ejoonie-dicom/00000024.dcm',
+    './fixture/ejoonie-dicom/00000025.dcm',
+    './fixture/ejoonie-dicom/00000026.dcm',
 ]
 
 files = file_names.map {|f| File.new(f, 'rb')}
 # 저장
-node  = DClient.new('localhost', 11112, {host_ae: 'DCM4CHEE'})
+node  = DClient.new(HOST, 11112, {host_ae: 'DCM4CHEE'})
 begin
   node.send(file_names)
 rescue Exception => e
@@ -63,7 +64,7 @@ end
 
 # 조회
 # https://github.com/dcm4che/dcm4chee-arc-light/wiki/RESTful-Services#wado_rs
-res = RestClient.get('http://0.0.0.0:8080/dcm4chee-arc/aets/DCM4CHEE/rs/studies')
-res = RestClient.get('http://0.0.0.0:8080/dcm4chee-arc/aets/DCM4CHEE/rs/studies/count')
-res = RestClient.get('http://0.0.0.0:8080/dcm4chee-arc/aets/DCM4CHEE/rs/instances/count')
+res = RestClient.get("http://#{HOST}:#{PORT}/dcm4chee-arc/aets/DCM4CHEE/rs/studies")
+res = RestClient.get("http://#{HOST}:#{PORT}/dcm4chee-arc/aets/DCM4CHEE/rs/studies/count")
+res = RestClient.get("http://#{HOST}:#{PORT}/dcm4chee-arc/aets/DCM4CHEE/rs/instances/count")
 p res.body
