692cca55b78bb1dceff4d44ea93b7f71f9a380f3
Youngjoon Choi <ejoonie@gmail.com>
2020-03-24 13:00:28 +0900

    comparing patients, files count

--- ruby_client/check.rb
+++ ruby_client/check.rb
@@ -2,11 +2,14 @@ require 'json'
 require './send_folder.rb'
 include SendFolder
 
-success = JSON.parse(File.read('success.json'))
-p "success.json count: #{success.size}"
+DATA_ROOT = "/usr/src/data"
+p "local patients count: #{Dir.glob('*', base: DATA_ROOT).length}"
+p "local file count: #{Dir.glob(File.join("**", "*.dcm"), base: DATA_ROOT).length}"
+
+# success = JSON.parse(File.read('success.json'))
+# p "success.json count: #{success.size}"
 
 p "server file count: #{current_count}"
-
-p "patients count: #{patients_count}"
+p "server patients count: #{patients_count}"
 
 
